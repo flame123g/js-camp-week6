fb6b05cd0b63e0cf97560f2d60f1d138c581c8ed
Ivy Huang <flame123g@gmail.com>
2026-04-13 21:04:04 +0800

    作業完成

--- homework.js
+++ homework.js
@@ -27,6 +27,10 @@ async function getProducts() {
 	// 1. 使用 fetch() 發送 GET 請求
 	// 2. 使用 response.json() 解析回應
 	// 3. 回傳 data.products
+	const url=`${BASE_URL}/api/livejs/v1/customer/${API_PATH}/products`;
+	const response=await fetch(url);
+	const data =await response.json();
+	return data.products;
 }
 
 /**
@@ -35,6 +39,14 @@ async function getProducts() {
  */
 async function getCart() {
 	// 請實作此函式
+	const url=`${BASE_URL}/api/livejs/v1/customer/${API_PATH}/carts`;
+	const response=await fetch(url);
+	const data =await response.json();
+	const obj={};
+	obj.carts=data.carts;
+	obj.total=data.total;
+	obj.finalTotal=data.finalTotal;
+	return obj;
 }
 
 /**
@@ -48,6 +60,24 @@ async function getProductsSafe() {
 	// 2. 檢查 response.ok 判斷是否成功
 	// 3. 成功回傳 { success: true, data: [...] }
 	// 4. 失敗回傳 { success: false, error: '錯誤訊息' }
+	try{
+		const url=`${BASE_URL}/api/livejs/v1/customer/${API_PATH}/products`;
+		const response=await fetch(url);
+		const data =await response.json();
+		if(response.ok){
+			return {
+				success:true,
+				data:data.products
+			};
+		}else{
+			return {
+				success:false,
+				error: '錯誤訊息'
+			};
+		};
+	}catch(error){
+		console.log("錯誤:",error.message);
+	};
 }
 
 // ========================================
@@ -67,6 +97,21 @@ async function addToCart(productId, quantity) {
 	// 2. body 格式：{ data: { productId: "xxx", quantity: 1 } }
 	// 3. 記得設定 headers: { 'Content-Type': 'application/json' }
 	// 4. body 要用 JSON.stringify() 轉換
+	const url=`${BASE_URL}/api/livejs/v1/customer/${API_PATH}/carts`;
+	let body={
+			data:{
+				"productId": productId,
+				"quantity": quantity
+			}
+		};
+	const response=await fetch(url,{
+		method:"POST",
+		headers: { 'Content-Type': 'application/json' },
+		body:JSON.stringify(body)
+	});
+	const data =await response.json();
+	return data;
+
 }
 
 /**
@@ -80,7 +125,21 @@ async function updateCartItem(cartId, quantity) {
 	// 提示：
 	// 1. 發送 PATCH 請求
 	// 2. body 格式：{ data: { id: "購物車ID", quantity: 數量 } }
-}
+	const url=`${BASE_URL}/api/livejs/v1/customer/${API_PATH}/carts`;
+	let body={
+			data:{
+				"id": cartId,
+				"quantity": quantity
+			}
+		};
+	const response=await fetch(url,{
+		method:"PATCH",
+		headers: { 'Content-Type': 'application/json' },
+		body:JSON.stringify(body)
+	});
+	const data =await response.json();
+	return data;
+};
 
 /**
  * 3. 刪除購物車特定商品
@@ -90,7 +149,14 @@ async function updateCartItem(cartId, quantity) {
 async function removeCartItem(cartId) {
 	// 請實作此函式
 	// 提示：發送 DELETE 請求到 /carts/{id}
-}
+	const url=`${BASE_URL}/api/livejs/v1/customer/${API_PATH}/carts/${cartId}`;
+	const response=await fetch(url,{
+		method:"DELETE",
+		headers: { 'Content-Type': 'application/json' }
+	});
+	const data =await response.json();
+	return data;
+};
 
 /**
  * 4. 清空購物車
@@ -99,6 +165,14 @@ async function removeCartItem(cartId) {
 async function clearCart() {
 	// 請實作此函式
 	// 提示：發送 DELETE 請求到 /carts
+	const url=`${BASE_URL}/api/livejs/v1/customer/${API_PATH}/carts`;
+	const response=await fetch(url,{
+		method:"DELETE",
+		headers: { 'Content-Type': 'application/json' }
+	});
+	const data =await response.json();
+	console.log(data);
+	return data;
 }
 
 // ========================================
@@ -110,13 +184,29 @@ async function clearCart() {
 
 1. HTTP 狀態碼的分類（1xx, 2xx, 3xx, 4xx, 5xx 各代表什麼）
    答：
+   1xx:資訊回應。例如:100表示請求已收到，繼續處理。
+   2xx:成功回應。例如200表示請求已收到，並成功回應。
+   3xx:重新導向訊息。例如304表示資源未修改，使用快取版本。
+   4xx:用戶端錯誤回應(前端請求錯誤)。例如400表示可能有語法上的錯誤，導致伺服器無法處理。
+   5xx:伺服器錯誤回應(後端請求錯誤)。例如500表示伺服器端發生未知或無法處理的錯誤。。
 
 2. GET、POST、PATCH、PUT、DELETE 的差異
    答：
+   以下皆是向伺服器提出請求，但動作有所不同:
+   GET:「查詢資料」的動作。
+   POST：「新增資料」的動作。
+   PATCH：「修改資料」的動作，針對一筆資料的某幾筆欄位進行編輯、修改。
+   PUT：「整份資料的修改」也就是Replace，針對一筆資料的「全部」欄位進行填寫。
+   DELETE：「資料的刪除」，針對資料進行刪除。
 
 3. 什麼是 RESTful API？
-   答：
-
+   答：就是用一個唯一的 URL 定位資源，將動作藏在 HTTP 的 method 裡面的一種撰寫API 的設計風格；
+   特色是網址相同、方法不同，舉例如下:
+   GET    /carts      
+   POST   /carts      
+   PATCH  /carts       
+   DELETE /carts
+   用不同HTTP 方法來表達動作，但網址統一。
 
 */
 
